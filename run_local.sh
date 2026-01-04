44b90d5f7495391e346db695f2d3b38e4c9a5fc2
aguszunda <agustin.zunda@gmail.com>
2026-01-03 21:12:04 -0300

    chore(docker): update db credentials and fix build path

--- run_local.sh
+++ run_local.sh
@@ -4,11 +4,11 @@
 # docker-compose up -d db
 
 # Run the application with local environment variables
-export DB_USER=user
-export DB_PASSWORD=password
+export DB_USER=root
+export DB_PASSWORD=admin2023
 export DB_HOST=localhost
 export DB_NAME=books_db
-export OTEL_EXPORTER_OTLP_ENDPOINT=localhost:4317
+export OTEL_EXPORTER_OTLP_ENDPOINT=localhost:3306
 
 echo "Starting api..."
 ./api
